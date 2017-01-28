a52e56b8f783ccd9750a067c1ed069ff35606f97
App Academy Student <student@appacademy.io>
2017-01-27 16:49:24 -0800

    complete kpf.rb

--- lib/kpf.rb
+++ lib/kpf.rb
@@ -14,44 +14,52 @@ class KnightPathFinder
     ]
 
   def initialize(arr)
-    puts "initializing"
     @visited_positions = [arr]
     @root = build_move_tree(PolyTreeNode.new(arr))
-     #Maybe nodes
-    #@position = arr
     @board = Array.new(8) { Array.new(8) }
   end
 
   def build_move_tree(root)
-    puts "building"
     queue = [root]
 
     until queue.empty?
       current_node = queue.shift
-      ##do something
       new_move_positions(current_node.value).each do |move|
         current_node.add_child(PolyTreeNode.new(move))
       end
 
       queue += current_node.children
     end
+    
+    root
   end
 
   def self.valid_moves(pos)
-    puts "valid moves"
     spots = MOVES.map { |el| [ el[0] + pos[0], el[1] + pos[1] ] }
-    puts "spots moves"
     spots.select { |el| ((0..7).include? el[0]) && ((0..7).include? el[1]) }
   end
 
   def new_move_positions(pos)
-    puts "new moves"
     new_positions = []
     KnightPathFinder.valid_moves(pos).each do |el|
-      puts "#{el} in #{new_positions} new positions"
       new_positions << el unless @visited_positions.include?(el)
     end
     @visited_positions.concat(new_positions)
     new_positions
+  end
+
+  def find_path(end_pos)
+    trace_path_back(@root.dfs(end_pos))
+  end
+
+  def trace_path_back(node)
+    result = [node.value]
+
+    current_node = node
+    until current_node == root
+      current_node = current_node.parent
+      result << current_node.value
+    end
+    result.reverse
   end
 end
