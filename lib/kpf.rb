bf339370b880c320854e82a6343c68365ddd4c4f
App Academy Student <student@appacademy.io>
2017-01-27 16:04:58 -0800

    Do build_move_tree

--- lib/kpf.rb
+++ lib/kpf.rb
@@ -14,20 +14,23 @@ class KnightPathFinder
     ]
 
   def initialize(arr)
+    puts "initializing"
+    @visited_positions = [arr]
     @root = build_move_tree(PolyTreeNode.new(arr))
-    @visited_positions = [arr] #Maybe nodes
+     #Maybe nodes
     #@position = arr
     @board = Array.new(8) { Array.new(8) }
   end
 
   def build_move_tree(root)
+    puts "building"
     queue = [root]
 
     until queue.empty?
       current_node = queue.shift
       ##do something
-      new_move_positions(root).each do |move|
-        root.add_child(PolyTreeNode.new(move))
+      new_move_positions(current_node.value).each do |move|
+        current_node.add_child(PolyTreeNode.new(move))
       end
 
       queue += current_node.children
@@ -35,13 +38,17 @@ class KnightPathFinder
   end
 
   def self.valid_moves(pos)
+    puts "valid moves"
     spots = MOVES.map { |el| [ el[0] + pos[0], el[1] + pos[1] ] }
+    puts "spots moves"
     spots.select { |el| ((0..7).include? el[0]) && ((0..7).include? el[1]) }
   end
 
   def new_move_positions(pos)
+    puts "new moves"
     new_positions = []
     KnightPathFinder.valid_moves(pos).each do |el|
+      puts "#{el} in #{new_positions} new positions"
       new_positions << el unless @visited_positions.include?(el)
     end
     @visited_positions.concat(new_positions)
